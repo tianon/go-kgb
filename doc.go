0cb3f832da5253d7812cffc58ef667c893eb3ca3
Tianon Gravi <admwiggin@gmail.com>
2017-02-11 21:21:17 -0800

    Add "doc.go"

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package kgb provides functions for interacting with "kgb-bot".
+
+See https://packages.debian.org/kgb-bot and/or https://kgb.alioth.debian.org/ for information about the upstream project.
+*/
+package kgb // import "go.tianon.xyz/kgb"
