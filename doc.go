aa3caa24ade76f400e71d048ad31bdc4ae7e7a0e
Tianon Gravi <admwiggin@gmail.com>
2017-02-11 21:23:19 -0800

    Add example code to "doc.go"

--- doc.go
+++ doc.go
@@ -2,5 +2,20 @@
 Package kgb provides functions for interacting with "kgb-bot".
 
 See https://packages.debian.org/kgb-bot and/or https://kgb.alioth.debian.org/ for information about the upstream project.
+
+	package main
+
+	import (
+		"go.tianon.xyz/kgb"
+	)
+
+	func main() {
+		project := kgb.NewClient("http://localhost:5391").Project("example-repo-id", "example-repo-password")
+
+		err := project.RelayMessage("hi y'all!")
+		if err != nil {
+			panic(err)
+		}
+	}
 */
 package kgb // import "go.tianon.xyz/kgb"
